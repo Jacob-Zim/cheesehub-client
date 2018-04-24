20bb76b1491fed36523876a806be3bb13b5fafea
JacobAZimmerman <ijacobzim@gmail.com>
2018-04-24 11:33:53 -0700

    google maps integration

--- src/components/spot-list.js
+++ src/components/spot-list.js
@@ -4,10 +4,18 @@ import {connect} from 'react-redux';
 
 import {fetchSpots} from '../actions/spot';
 
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
 export class SpotList extends React.Component {
 
     componentDidMount() {
         this.props.dispatch(fetchSpots());
+    }
+
+    getClickedPosition(event) {
+        const lat = event.latLng.lat();
+        const lng = event.latLng.lng();
+        console.log(lat,lng);
     }
 
     render() {
@@ -15,13 +23,27 @@ export class SpotList extends React.Component {
         if (this.props.spotList) {
             console.log(this.props);
             if (this.props.spotList.spots) {
-                list =  this.props.spotList.spots.map((spot, key) => <li key={key}>{spot.location}</li>);
+                list =  this.props.spotList.spots.map((spot) => <Marker position={{ lat: spot.lat, lng: spot.lng }}></Marker>);
             }
         }
+        const MyMap = withScriptjs(withGoogleMap((/*PROPS HERE*/) => 
+                <GoogleMap 
+                defaultZoom={8}
+                defaultCenter={{ lat: -34.397, lng: 150.64 }}
+                onClick={e => this.getClickedPosition(e)}
+                >
+                    {list}
+                </GoogleMap>
+                ));
         return (
-            <ul>
-                {list}
-            </ul>
+            <div className="map">
+            <MyMap
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD9t7sxzQrg5KAG4FG7CFbX-ghGbqtktH0&v=3.exp&libraries=geometry,drawing,places"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `600px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
+            </div>
         );
     }
 }
