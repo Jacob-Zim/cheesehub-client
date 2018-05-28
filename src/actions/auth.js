23d6338121e95a79ce224f16401561364a2376b5
JacobAZimmerman <ijacobzim@gmail.com>
2018-05-27 17:23:00 -0700

    register submit edits

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -81,7 +81,6 @@ export const login = (username, password) => dispatch => {
             .then(res => res.json())
             .then(({authToken}) => storeAuthInfo(authToken, dispatch))
             .catch(err => {
-                console.log(err);
                 dispatch(authError(err.message));
             })
     );
